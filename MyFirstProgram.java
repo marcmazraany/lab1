e96b609d8cdf060800f4e46c8d0e35c8b073b2c9
Marc Mazraany <marc.mazraany@lau.edu>
2023-01-26 19:18:24 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+	public static void main(String[] args){
+		System.out.print("this is my ");
+		System.out.println("first java program");
+		System.out.println("programing is fun!");
+	}
+}
